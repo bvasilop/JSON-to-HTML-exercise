92bc4ac859ea90e65348efb5c9cb4f65fa045797
bvasilop <bvasilop@gmail.com>
2019-06-26 13:16:51 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+
+window.onload = function() {
+const user_profile = {
+    username: 'Sammy Jones',
+    social_media: [
+        {
+            description: 'twitter',
+            link: 'https://twitter.com/',
+        },
+        {
+            description: 'facebook',
+            link: 'https://www.facebook.com/',
+        },
+        {
+            description: 'github',
+            link: 'https://github.com/',
+        },
+        {
+            description: 'instagram',
+            link: 'https://instagram.com/',
+        },
+    ],
+};
+
+document.querySelector('.username').innerHTML =
+    // user name
+    user_profile.username + '<br>';
+
+user_profile.social_media.forEach(function(item) {
+document.querySelector('.social-media').innerHTML +=
+item.description + '<br>' +
+item.link + '<br>' + '<hr>';
+});
+
+// Example using arrays
+
+// user_profile.social_media.description +
+// '<br>';
+
+    // // twitter
+    // user_profile.social_media[0].description +
+    // '<br>' +
+    // user_profile.social_media[0].link +
+    // '<hr>' +
+    // // facebook
+    // user_profile.social_media[1].description +
+    // '<br>' +
+    // user_profile.social_media[1].link +
+    // '<hr>' +
+    // // github
+    // user_profile.social_media[2].description +
+    // '<br>' +
+    // user_profile.social_media[2].link +
+    // '<hr>' +
+    // // instagram
+    // user_profile.social_media[3].description +
+    // '<br>' +
+    // user_profile.social_media[3].link;
+};
